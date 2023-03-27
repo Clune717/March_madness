97e713af5783541925bbff52c4cc3c1986db0605
Alexander Parisi <alexanderparisi@Alexanders-MacBook-Air-2.local>
2023-03-27 15:04:26 -0400

    alex commit

---
+++ march_madness_front/src/App.js
@@ -0,0 +1,16 @@
+import {useState} from 'react'
+
+const App = () => {
+
+  const [teams, setTeam] = useState([])
+
+
+
+  return (
+  <>
+    <h1>March Madness Teams</h1>
+  </>
+  )
+}
+
+export default App
